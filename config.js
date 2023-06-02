5a364f9017d3d9851a491557986f6fc9948142c3
VivaanSejpal <108406192+VivaanSejpal@users.noreply.github.com>
2023-06-02 17:13:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+/* This code is exporting a constant variable named `firebaseConfig` which contains the configuration
+details required to connect to a Firebase project. These details include the API key, authentication
+domain, project ID, storage bucket, messaging sender ID, and app ID. This exported variable can be
+imported and used in other parts of the codebase to connect to the Firebase project. */
+export const firebaseConfig = {
+  apiKey: "AIzaSyCUw7o6TEhBZVlXyxWzyIbAdvjnm13WTOI",
+  authDomain: "storytellingapp-52386.firebaseapp.com",
+  projectId: "storytellingapp-52386",
+  storageBucket: "storytellingapp-52386.appspot.com",
+  messagingSenderId: "169067545995",
+  appId: "1:169067545995:web:55571d03a9f684bee79b8e"
+  };
